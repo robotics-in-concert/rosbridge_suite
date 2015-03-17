d1a6a6432a46ebfff04a99d8ac47439b3f1cea87
Ekumen <pablorm@creativa77.com.ar>
2015-03-17 12:42:07 -0300

    Changed tab to spaces

--- rosbridge_server/scripts/rosbridge_websocket_client.py
+++ rosbridge_server/scripts/rosbridge_websocket_client.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
         server_uri = server_uri.replace("8888", "9090")
         # In the future we are going need to use everithing on the same port
         # given throught the argument
-	ws = WebsocketClientTornado(server_uri)
+        ws = WebsocketClientTornado(server_uri)
         protocol.outgoing = ws.send_message
 
         # Loop
