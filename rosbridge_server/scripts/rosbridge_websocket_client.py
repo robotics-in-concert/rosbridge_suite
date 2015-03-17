f9710e2bc6426d5eda90be0edf983195aed84641
Ekumen <pablorm@creativa77.com.ar>
2015-03-17 13:14:26 -0300

    Now you have to pass the complete server uri to the client

--- rosbridge_server/scripts/rosbridge_websocket_client.py
+++ rosbridge_server/scripts/rosbridge_websocket_client.py
@@ -127,8 +127,7 @@ if __name__ == "__main__":
         protocol = RosbridgeProtocol(0)
 
         # Connect with server
-        server_uri = "ws://" + rospy.get_param("~webserver_uri") + "/ws"
-        server_uri = server_uri.replace("8888", "9090")
+        server_uri = rospy.get_param("~webserver_uri")
         # In the future we are going need to use everithing on the same port
         # given throught the argument
         ws = WebsocketClientTornado(server_uri)
